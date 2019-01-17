8a921ff5a3206b485c9f09ac91ac0770b17e04b8
Sergio Miracco <sergio.miracco@gmail.com>
2019-01-17 20:44:51 +0100

    define assignments according to crankshaft definition

---
+++ assignments.h
@@ -0,0 +1,42 @@
+
+enum struct key : char
+{
+  _ENTER =        KEY_RETURN,
+  _LEFT =         KEY_LEFT_ARROW,
+  _RIGHT =        KEY_RIGHT_ARROW,
+  _UP =           KEY_UP_ARROW,
+  _DOWN =         KEY_DOWN_ARROW,
+  _BACK =         KEY_ESC,
+  _HOME =         'H',
+  _PHONE =        'P',
+  _CALL_END =     'O',
+  _PLAY =         'X',
+  _PAUSE =        'C',
+  _PREV_TRACK =   'V',
+  _NEXT_TRACK =   'N',
+  _TOGGLE_PLAY =  'B',
+  _VOICE_CMD =    'M',
+  _WHEEL_LEFT =   '1',
+  _WHEEL_RIGHT =  '2',
+}
+
+enum struct input_pin : uint8_t 
+{
+  _ENTER =        0,
+  _LEFT =         1,
+  _RIGHT =        2,
+  _UP =           3,
+  _DOWN =         4,
+  _BACK =         5,
+  _HOME =         6,
+  _PHONE =        7,
+  _CALL_END =     8,
+  _PLAY =         9,
+  _PAUSE =        10,
+  _PREV_TRACK =   11,
+  _NEXT_TRACK =   12,
+  _TOGGLE_PLAY =  13,
+  _VOICE_CMD =    14,
+  _WHEEL_LEFT =   15,
+  _WHEEL_RIGHT =  16,
+}
