bc599b9b17890be51ede1e909c4e853a61341f8d
Sergio Miracco <sergio.miracco@gmail.com>
2019-01-17 20:45:52 +0100

    add keyboard dependency and assignments

--- crankAMI.ino
+++ crankAMI.ino
@@ -1,9 +1,10 @@
+#include <Keyboard.h>
+#include "assignments.h"
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
